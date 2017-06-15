3b5b2d8d1f45e26ee90754612daa29455a42feef
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-15 13:47:02 -1000

    modified

--- secret_number.js
+++ secret_number.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function() {
-  let secretNumber = (Math.random()*1000000 + 1);
+  let secretNumber = ((Math.random()*1000001));
   return function(){
     return secretNumber;
   };
