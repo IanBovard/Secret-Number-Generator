9682cc4e364091283891b785b11270f00c2c13f6
Ian Bovard <thebestpantsintown@gmail.com>
2017-06-15 12:51:55 -1000

    complete

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,15 @@
 'use strict';
 module.exports = function() {
-  
+  let secretNumber = (Math.random()*1000000 + 1);
+  return function(){
+    return secretNumber;
+  };
 };
+var myNumber = module.exports();
+console.log(myNumber());
+console.log(myNumber());
+console.log(myNumber());
+var myNewNumber = module.exports();
+console.log(myNewNumber());
+console.log(myNewNumber());
+console.log(myNewNumber());
